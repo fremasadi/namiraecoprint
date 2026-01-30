ed38180b2911998a7739de75092a0dc0f41ce667
fremasadi <fremasadii@gmail.com>
2026-01-30 09:20:25 +0700

    feat

--- app/Filament/Resources/Abouts/Pages/EditAbout.php
+++ app/Filament/Resources/Abouts/Pages/EditAbout.php
@@ -1,7 +1,5 @@
     <?php
 
-    namespace App\Filament\Resources\Abouts\Pages;
-
     use App\Filament\Resources\Abouts\AboutResource;
     use App\Models\About;
     use Filament\Resources\Pages\EditRecord;
